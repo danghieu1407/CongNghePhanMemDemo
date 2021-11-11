c3213ff5ed6bae9ceaf948e2e81000ac2af02542
unknown <cbt270801@gmail.com>
2021-11-11 09:35:24 +0700

    Dieu Huong du lieu

---
+++ Frontend/AdminManager/Content.php
@@ -0,0 +1,41 @@
+
+<?php
+    if(isset($_GET['manage']))
+        {
+        $manage = $_GET['manage'];
+        }
+    else
+        {
+        $manage='';
+        }
+
+    if(isset($_GET['action']))
+        {
+        $action = $_GET['action'];
+        }
+    else
+        {
+        $action='';
+        }
+
+
+   
+        if ($manage=="RoomManage")
+        {
+          include('ContentPage/RoomManager.php');
+        }
+        else if($manage=="RoomSetting")
+        {
+            include('ContentPage/RoomSetting.php');
+        }
+        else if($manage=="RoomOdered")
+        {
+            include('ContentPage/RoomOdered.php');
+        }
+        else
+        {
+          include('ContentPage/RoomManager.php');
+        }
+        
+        
+    ?>
--- Frontend/AdminManager/ContentPage/RoomManager.php
+++ Frontend/AdminManager/ContentPage/RoomManager.php
@@ -1,69 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Khách Sạn To Đùng</title>
-	<link rel="stylesheet" href="fontawesome/css/all.min.css"> <!-- https://fontawesome.com/ -->
-	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> <!-- https://fonts.google.com/ -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="css/templatemo-xtra-blog.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Style/style.css">
-
-
-</head>
-<body>
-	<header class="tm-header" id="tm-header">
-        <div class="">
-            <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="tm-site-header">
-                <div class="mb-3 mx-auto tm-site-logo"><i class="fas fa-times fa-2x"></i></div>            
-                <h1 class="text-center">Admin</h1>
-            </div>
-            <nav class="tm-nav" id="tm-nav">            
-                <ul>
-                    <li class="tm-nav-item active"><a href="#" class="tm-nav-link">
-                        <i class="fas fa-home"></i>
-                        Quản lý Phòng
-                    </a></li>
-                    <li class="tm-nav-item active"><a href="#" class="tm-nav-link">
-                        <i class="fas fa-home"></i>
-                        Phòng đặt
-                    </a></li>
-                    <li class="tm-nav-item active"><a href="#" class="tm-nav-link">
-                        <i class="fas fa-home"></i>
-                        Kênh chat 
-                    </a></li>
-                    <li class="tm-nav-item active"><a href="#" class="tm-nav-link">
-                        <i class="fas fa-home"></i>
-                        Chỉnh sửa phòng
-                    </a></li>
-                  
-                  
-                </ul>
-            </nav>
-      
-    </header>
-    <div class="container-fluid">
-        <main class="tm-main">
-            <!-- Search form -->
-            <div class="row tm-row">
-                <div class="col-12">
-                    <form method="GET" class="form-inline tm-mb-80 tm-search-form">                
-                        <input class="form-control tm-search-input" name="query" type="text" placeholder="Search..." aria-label="Search">
-                        <button class="tm-search-button" type="submit">
-                            <i class="fas fa-search tm-search-icon" aria-hidden="true"></i>
-                        </button>                                
-                    </form>
-                </div>                
-            </div>            
-            <div class="row tm-row ">
+<div class="row tm-row ">
                 <h1> Tầng 1</h1>
 
                 <article class="col-12 col-md-6 tm-post">
@@ -151,11 +86,3 @@
                 </article>
               
             </div>
-                    
-                      
-         
-        </main>
-    </div>
-    
-</body>
-</html>
