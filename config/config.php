8e580afe7e6ce30dd180ba22967ef6cc49d0c6ce
danghieu1407 <danghieu140701@gmail.com>
2021-11-13 20:50:29 +0700

    complete login

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-    $mysqli = new mysqli('localhost','root','','CongNghePhanMemDemo');
+$mysqli = new mysqli("localhost","root","","bighotel");
 
-    if($mysqli-> connect_error)
-    {
-        echo "Failed to connect to MYSQL: " . $mysqli->connect_error;
-        exit();
-    }
-?> 
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+?>
