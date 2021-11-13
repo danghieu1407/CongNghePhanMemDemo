337ca4d572e9cd1cd1253bc40041a475011e893e
danghieu1407 <danghieu140701@gmail.com>
2021-11-13 22:47:41 +0700

    sign up complete

--- Frontend/signUp.php
+++ Frontend/signUp.php
@@ -4,13 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../Style/login.css">
-    <title>Đăng nhập</title>
+    <title>Đăng kí</title>
 </head>
 
 <body>
@@ -18,26 +17,32 @@
     <div class="container">
         <div class="card-login">
             <div class="header">
-                <h2>Đăng Nhập</h2>
+                <h2>Đăng kí</h2>
             </div>
             <div class="input">
-                <form action="../Backend/loginProcess.php" method="POST">
-                    <input type="text" name="Email" placeholder="Tên đăng nhập" required>
-                    <input type="password" name="Pass" placeholder="Mật khẩu" required>
+                <form action="../Backend/singUpProcess.php" method="POST">
+                    <input type="text" name="Username" placeholder="Tên khách hàng đặt phòng" required>
+                    <input type="password" name="Password" placeholder="Mật khẩu" required>
+                    <!-- <span style="font-size:15px; color:red"></span> -->
+                    <input type="text" name="Email" placeholder="Địa chỉ email" required>
+                    <input type="text" name="phoneNumber" placeholder="Số điện thoại di động" required>
                     <div class="text-danger small text-left mt-3">
                             <span id="errorMessage"><?php if (isset($_GET["msg1"])) echo $_GET["msg1"]; ?></span></td>
                         </div>
-                    <input type="submit" name="Login" value="Đăng Nhập">
-                    <a href="signUp.php">Chưa có tài khoản</a>
-
-                        
+                        <div class="text-danger small text-left mt-3">
+                            <span id="errorMessage"><?php if (isset($_GET["msg2"])) echo $_GET["msg2"]; ?></span></td>
+                        </div>
+                    
+                    <input type="submit" name="signup" value="Đăng kí">
+                    
+                    
                 </form>
+
             </div>
         </div>
     </div>
     
-
-  <?php
+    <?php
 include('Dialog.php') ;
 ?>
 </body>
