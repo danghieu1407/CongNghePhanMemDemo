397eae4aa8f0e2efa87848292bec6f5f34cb7bea
unknown <cbt270801@gmail.com>
2021-11-13 12:34:35 +0700

    Update chuc nang

--- Frontend/AdminManager/ContentPage/managementStaff.php
+++ Frontend/AdminManager/ContentPage/managementStaff.php
@@ -1,4 +1,13 @@
-
+<div class="row tm-row">
+                <div class="col-12">
+                    <form method="GET" class="form-inline tm-mb-80 tm-search-form">                
+                        <input class="form-control tm-search-input" name="query" type="text" placeholder="Search..." aria-label="Search">
+                        <button class="tm-search-button" type="submit">
+                            <i class="fas fa-search tm-search-icon" aria-hidden="true"></i>
+                        </button>                                
+                    </form>
+                </div>                
+            </div>
 <div class="Staff">
                 <table>
                     <tr class="tr-Staff">
@@ -6,6 +15,7 @@
                         <th>Số điện thoại </th>
                         <th>Lương</th>
                         <th>Chức vụ</th>
+                        <th>Thao tác</th>
                     </tr>
                    
                 </table>
